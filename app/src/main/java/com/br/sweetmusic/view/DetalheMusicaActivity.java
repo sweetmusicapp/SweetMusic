0695a85302edf7a3ef0dd6d275e8345809b98ff1
DiegoHonorio <honorioproduction@gmail.com>
2019-11-24 17:21:28 -0300

    Corrigido

--- app/src/main/java/com/br/sweetmusic/view/DetalheMusicaActivity.java
+++
@@ -1,7 +0,0 @@
-package com.br.sweetmusic.view;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class DetalheMusicaActivity extends AppCompatActivity {
-
-}
--- app/src/main/java/com/br/sweetmusic/view/login/SplashActivity.java
+++ app/src/main/java/com/br/sweetmusic/view/login/SplashActivity.java
@@ -7,49 +7,41 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.br.sweetmusic.MainActivity;
 import com.br.sweetmusic.R;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
-
     private ImageView imgSplash;
-
     private Timer timer = new Timer();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        getSupportActionBar().hide(); // tira a AppBar
-
         imgSplash = findViewById(R.id.imageView_splash_logo);
 
         imgSplash.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                jump();
+            public void onClick(View view) {
+                nextScreen();
             }
         });
 
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                jump();
+                nextScreen();
             }
-        }, 2000);
+        }, 3000);
     }
 
-    private void jump(){
+    public void nextScreen() {
         timer.cancel();
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
         finish();
     }
 
 }
-
-
