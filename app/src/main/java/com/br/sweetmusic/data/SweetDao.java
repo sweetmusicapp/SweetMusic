da76dbc31b4ad9210f5c20dcd7185f2f92369d86
Victor Amaral <vic_amaral_23@hotmail.com>
2019-12-01 21:19:56 -0300

    Adicionado método que traz apenas os artistas favoritos;

--- app/src/main/java/com/br/sweetmusic/data/SweetDao.java
+++ app/src/main/java/com/br/sweetmusic/data/SweetDao.java
@@ -9,6 +9,8 @@ import androidx.room.Update;
 import com.br.sweetmusic.pojos.Artist;
 import com.br.sweetmusic.pojos.Track;
 
+import static android.icu.text.MessagePattern.ArgType.SELECT;
+
 @Dao
 public interface SweetDao {
 
@@ -21,8 +23,12 @@ public interface SweetDao {
     @Update
     void updateArtista(Artist artista);
 
+    //Retorna o artista do DB de acordo com o nome
     @Query("SELECT * FROM artist WHERE artist_nome = :nomeArtista")
     Artist getArtistByName(String nomeArtista);
 
+    //Retorna apenas os artistas do DB com favorito = 1
+    @Query("SELECT * FROM artist WHERE favorito = 1")
+    Artist getFavoritos();
 }
 
